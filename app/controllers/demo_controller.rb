b6f3c4721178fad6e19f0985956390c8fa51892c
Tarak321 <tarak3177@gmail.com>
2021-01-13 10:55:44 +0530

    added databses support

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,21 @@
+class DemoController < ApplicationController
+
+  layout false
+  
+  def index
+    render('index')
+  end
+  
+  def hello
+    @array = [1,2,3,4]
+    render('hello')
+  end
+
+  def other_hello
+    redirect_to(:controller => 'demo',:action => 'hello')
+  end
+  
+  def lynda
+    redirect_to('http://lynda.com')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :posts
+  root 'demo#index'
+
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+  # get ':controller(/:action(/:id))'
+  # may go away in future versions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
